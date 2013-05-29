c7d996a7904f57b3d22342666e66b5d5d239f824
Nick Martin <martin.nick@gmail.com>
2013-05-29 17:31:23 -0400

    Public secrets.

--- meteor-security-demo.js
+++ meteor-security-demo.js
@@ -18,19 +18,42 @@ if (Meteor.isClient) {
 
   // Returns all the secrets for the currently logged in user.
   Template.secrets.secrets = function() {
-    return Secrets.find();
+    return Secrets.find({userId: Meteor.userId()});
   };
 
   Template.secrets.events({
     // Removes the secret associated with the clicked delete button.
-    'click input': function(event) {
-      var id = event.toElement.id;
+    'click .removeSecretBtn': function(event) {
+      var id = event.srcElement.dataset.secret;
       Secrets.remove(id);
+    },
+    'click .publishSecretBtn': function(event) {
+      var id = event.srcElement.dataset.secret;
+      Secrets.update(id, {$set:{"public":true}});
+    }
+  });
+
+  // Returns all the secrets marked as public.
+  Template.publicSecrets.secrets = function() {
+    return Secrets.find({public: true});
+  };
+
+  Template.publicSecrets.events({
+    'click .privateSecretBtn': function() {
+    var id = event.srcElement.dataset.secret;
+    Secrets.update(id, {$set:{"public":false}}, function (error) {
+      if (error) {
+        alert("You can't make someone else's secrets private!");
+      }
+    });
     }
   });
 
   // Subscribes us to the 'yourSecrets' subscription.
   Meteor.subscribe('yourSecrets');
+
+  // Subscribes us to the 'publicSecrets' subscription.
+  Meteor.subscribe('publicSecrets');
 }
 
 // All of our server-side functionality.
@@ -44,11 +67,19 @@ if (Meteor.isServer) {
     },
     remove: function (userId, doc) {
       return (userId && userId == doc.userId) ? true : false;
+    },
+    update: function (userId, doc, fieldNames, modifier) {
+      return (userId && userId == doc.userId && fieldNames[0] == 'public') ? true : false;
     }
   });
 
   // Publish all of the secrets for the logged-in user as 'yourSecrets'.
   Meteor.publish('yourSecrets', function() {
     return Secrets.find({userId: this.userId});
+  });
+
+  // Publish all secrets that have been made public as 'publicSecrets'.
+  Meteor.publish('publicSecrets', function() {
+    return Secrets.find({public: true});
   });
 }
